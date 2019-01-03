7e3f41d46e342e56be4871b5b298527f54781658
EunHyunCho <ouoyy6395@gmail.com>
2019-01-03 14:37:42 +0900

    fixed #1 스켈레톤 작성

---
+++ src/main/java/my/miniproject/chatClient/ChatClientMain.java
@@ -0,0 +1,7 @@
+package my.miniproject.chatClient;
+
+public class ChatClientMain {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/main/java/my/miniproject/chatClient/ChatHouse.java
@@ -0,0 +1,4 @@
+package my.miniproject.chatClient;
+
+public class ChatHouse {
+}
